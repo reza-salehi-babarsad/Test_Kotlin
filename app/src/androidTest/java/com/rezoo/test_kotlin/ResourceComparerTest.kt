aa22627bc1135aaa6187425fde01563523bb765f
reza <rezasalehibabarsad@hotmail.com>
2023-07-16 10:26:29 +0330

    add @Before

--- app/src/androidTest/java/com/rezoo/test_kotlin/ResourceComparerTest.kt
+++ app/src/androidTest/java/com/rezoo/test_kotlin/ResourceComparerTest.kt
@@ -3,11 +3,16 @@ package com.rezoo.test_kotlin
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 class ResourceComparerTest{
 
-      private  val resourceComparer = ResourceComparer()
+      private  lateinit var resourceComparer:ResourceComparer
+      @Before
+      fun setup(){
+          resourceComparer= ResourceComparer()
+      }
 
     @Test
     fun stringResourceSameAsGivenString_returnsTrue(){
